bd10bf9c87c2d6c07a1d7cfd2f70860aa7e483fb
Luiz Felipe Moraes Lima <luiz.felipe.moraes.lima.1@gmail.com>
2022-04-25 14:55:01 -0300

    Urna Finalizada

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Rambo Regaça',
+                partido: 'Tiro',
+                fotos: [
+                    {url: 'Rambo.jpg', legenda: 'VEREADOR'}
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Pablo Escobar',
+                partido: 'NARCO',
+                fotos:[
+                    {url: 'pablo.jpeg', legenda: 'VEREADOR'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '69',
+                name: 'Chuck Norris',
+                partido: 'Tiro',
+                vice: 'Chapolin',
+                fotos: [
+                    {url: 'chuck.jpg', legenda: 'Prefeito'},
+                    {url: 'chapolin.jpg', legenda: 'Vice Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                name: 'Capitão Nascimento',
+                partido: 'Narco',
+                vice: 'Dadinho',
+                fotos: [
+                    {url: 'capNas.jpg', legenda: 'Prefeito'},
+                    {url: 'dadinho.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
